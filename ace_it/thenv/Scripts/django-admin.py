266d4c200d366034970d171ed4c2922df9406037
chuky <chukyjack@gmail.com>
2019-09-21 23:33:38 -0700

    'latest'

--- ace_it/thenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\owner\mystuff\ace_it_dataconsole\ace_it\thenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
